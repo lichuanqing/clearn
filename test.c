3b6c0fbc7130ccca1f40ecfb0164084c215a9f33
Li Chuanqing <lichuanqing.123@163.com>
2015-09-23 22:21:16 +0800

    使用字符常量定义 #define

--- test.c
+++ test.c
@@ -1,4 +1,9 @@
+#define PI 3.1415
 main()
 {
-    printf("hello!,haha");
+    float R,Area;
+    R=12.0;
+    printf("hello!\n");
+    Area=PI*R*R;
+    printf("Area of circle with radius %.1f is %.2f.\n" ,R,Area);
 }
