83386cce2bda6b26c7c724abc7a98962654394df
Li Chuanqing <lichuanqing.123@163.com>
2015-09-25 16:56:05 +0800

    Excerse for Chapter1

---
+++ Chapter1/Exe_1_01.c
@@ -0,0 +1,8 @@
+/* 练习1.1，C语言入门经典第四版 p.18 */
+#include <stdio.h>
+int main(void)
+{
+    printf("姓名：李传庆\n");
+    printf("地址：上海市浦东新区民生路600号\n");
+    return 0;
+}
---
+++ Chapter1/Exe_1_03.c
@@ -0,0 +1,8 @@
+/* 练习1.3，C语言入门经典第四版 p.18 */
+#include <stdio.h>
+int main(void)
+{
+    printf("\"It's freezing in here,\" he said coldly.");
+
+    return 0;
+}
