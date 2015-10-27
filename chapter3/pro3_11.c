15a8d00bf3b7fa467ea525a38f42d21cbdd5748c
Li Chuanqing <lichuanqing.123@163.com>
2015-10-27 16:41:01 +0800

    mistake: scanf,&;and add print for checking the read

--- chapter3/pro3_11.c
+++ chapter3/pro3_11.c
@@ -6,12 +6,12 @@ int main(void)
 {
 
 /* 变量定义*/
-double num1=0.0,mum2=0.0;
+double num1=0.0,num2=0.0;
 char ope=0; /* 运算符 必须是 + - * / */
 
 /* 赋值-输入*/
 printf("\n 这是一个四则运算的计算器，请输入计算表达式：\n");
-printf("%lf %c %lf",num1,ope,num2);
-
+scanf("%lf %c %lf",&num1,&ope,&num2);
+printf("x1=%lf,ope=%c,x2=%lf,",num1,ope,num2);
     return 0;
 }
