844ef646b642e8b68d84dc9d20afbf5cc08e9f83
Li Chuanqing <lichuanqing.123@163.com>
2015-10-27 16:35:08 +0800

    First read the input

---
+++ chapter3/pro3_11.c
@@ -0,0 +1,17 @@
+
+/* 程序实例3.11，章末程序设计-一个计算器
+*/
+#include<stdio.h>
+int main(void)
+{
+
+/* 变量定义*/
+double num1=0.0,mum2=0.0;
+char ope=0; /* 运算符 必须是 + - * / */
+
+/* 赋值-输入*/
+printf("\n 这是一个四则运算的计算器，请输入计算表达式：\n");
+printf("%lf %c %lf",num1,ope,num2);
+
+    return 0;
+}
